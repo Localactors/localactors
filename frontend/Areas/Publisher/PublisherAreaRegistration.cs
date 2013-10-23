208571f409eba3142c5730d688cfa3b1766ebe48
Diego <diego.trinciarelli@gmail.com>
2013-10-23 12:17:03 +0200

    Publisher Area

---
+++ frontend/Areas/Publisher/PublisherAreaRegistration.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace Localactors.webapp.Areas.Publisher
+{
+    public class PublisherAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Publisher";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            context.MapRoute(
+                "Publisher_default_home",
+                "/Publisher",
+                new {controller="home", action = "Index", id = UrlParameter.Optional }
+           );
+
+            context.MapRoute(
+                "Publisher_default",
+                "Publisher/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
